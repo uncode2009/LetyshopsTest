0cd36a97d624c46e081ff983d7db11f0a78ca49f
Vadim <uncode2009@gmail.com>
2017-05-28 12:40:54 +0300

    bower_components.js minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,23 @@
-var gulp    = require('gulp'), // Подключаем Gulp
+var gulp = require('gulp'),
   connect = require('gulp-connect'),
   sass = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
   minifyCSS = require('gulp-clean-css'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify');
+
 var startFolder = 'src/',
-finishFolder = 'dist/';
+  finishFolder = 'dist/';
 
 gulp.task('connect', function() {
   connect.server({
     root: finishFolder,
-    port:8080,
+    port: 8080,
     livereload: true,
     livereloadPort: 3000,
-    middleware:function(connect){
-    return[connect().use("/bower_components", connect.static("bower_components"))]; 
-   
+    middleware: function(connect) {
+      return [connect().use("/bower_components", connect.static("bower_components"))];
+
 
     }
   });
@@ -30,7 +32,7 @@ gulp.task('html-include', function() {
   return gulp.src(startFolder + 'modules/**/**/**.html')
     .pipe(gulp.dest(finishFolder + 'views/'));
 });
-gulp.task('sass', ['bower_styles','images'], function() {
+gulp.task('sass', ['bower_styles', 'images'], function() {
   return gulp.src(startFolder + 'sass/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
@@ -38,18 +40,18 @@ gulp.task('sass', ['bower_styles','images'], function() {
       cascade: false
     }))
     .pipe(concat('style.css'))
-    .pipe(minifyCSS({compatibility: 'ie8'}))
+    .pipe(minifyCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest(finishFolder + 'css/'))
     .pipe(connect.reload());
 });
 gulp.task('bower_styles', function() {
   gulp.src(startFolder + 'css/bootstrap.css')
     .pipe(concat('bower_components.css'))
-    .pipe(minifyCSS({compatibility: 'ie8'}))
+    .pipe(minifyCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest(finishFolder + 'css/'));
 });
 
- gulp.task('images', function() {
+gulp.task('images', function() {
   return gulp.src(startFolder + 'img/**/*')
     .pipe(gulp.dest(finishFolder + 'img/'));
 });
@@ -64,8 +66,6 @@ gulp.task('js', function() {
       startFolder + '**/**/**/**/**.controller.js'
     ])
     .pipe(concat('app.js'))
-  /*  .pipe(minify())
-    .pipe(uglify())*/
     .pipe(gulp.dest(finishFolder + 'js/'))
     .pipe(connect.reload());
 
@@ -75,10 +75,9 @@ gulp.task('js', function() {
       'bower_components/angularUtils-pagination/dirPagination.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
       'bower_components/angular-bootstrap/ui-bootstrap.js'
-      ])
+    ])
     .pipe(concat('bower_components.js'))
- /*   .pipe(minify())
-    .pipe(uglify())*/
+    .pipe(uglify())
     .pipe(gulp.dest(finishFolder + 'js'));
 });
 gulp.task('watch', function() {
@@ -92,5 +91,3 @@ gulp.task('build', ['html', 'sass', 'js'], function() {
 
 });
 gulp.task('default', ['connect', 'watch', 'build']);
-
-
